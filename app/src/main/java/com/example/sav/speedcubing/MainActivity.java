976b65ed7612c520b6bca7e3930f1585348caada
Sav <a.sipnevski@gmail.com>
2016-02-13 12:06:29 +0300

    some more job to be done to make it work also should move some methods to separate class

--- app/src/main/java/com/example/sav/speedcubing/MainActivity.java
+++ app/src/main/java/com/example/sav/speedcubing/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.sav.speedcubing;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tempTextView; //Temporary TextView
+    private Button tempBtn; //Temporary Button
+    private Handler mHandler = new Handler();
+
+    private long startTime;
+    private long elapsedTime;
+    private long secs, mins, msecs;
+    private String hours, minutes, seconds, milliseconds;
+    private boolean stopped = false;
+    private final int REFRESH_RATE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +30,99 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
+
+
+    private void showStopButton(){
+        ((Button)findViewById(R.id.startButton)).setVisibility(View.GONE);
+        //((Button)findViewById(R.id.resetButton)).setVisibility(View.GONE);
+        ((Button)findViewById(R.id.stopButton)).setVisibility(View.VISIBLE);
+    }
+
+    private void hideStopButton(){
+        ((Button)findViewById(R.id.startButton)).setVisibility(View.VISIBLE);
+        //((Button)findViewById(R.id.resetButton)).setVisibility(View.VISIBLE);
+        ((Button)findViewById(R.id.stopButton)).setVisibility(View.GONE);
+    }
+
+    private void updateTimer(float time) {
+        msecs = (long) (time);
+        secs = (long) (time / 1000);
+        mins = (long) ((time / 1000) / 60);
+
+        // Convert seconds to String
+        secs = secs % 60;
+        seconds = String.valueOf(secs);
+        if (secs == 0) {
+            seconds = "00";
+        }
+        if (secs < 10 && secs > 0) {
+            seconds = "0" + seconds;
+        }
+        // Convert minutes to String and format the String
+        mins = mins % 60;
+        minutes = String.valueOf(mins);
+        if (mins == 0) {
+            minutes = "00";
+        }
+        if (mins < 10 && mins > 0) {
+            minutes = "0" + minutes;
+        }
+
+        // Convert milliseconds to String and format
+        milliseconds = String.valueOf((long) time);
+        if (milliseconds.length() == 2) {
+            milliseconds = "0" + milliseconds;
+        }
+        if (milliseconds.length() <= 1) {
+            milliseconds = "00";
+        }
+        milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+
+        /* Setting the timer text to the elapsed time */
+        ((TextView) findViewById(R.id.timer)).setText(hours + ":" + minutes + ":" + seconds);
+        ((TextView) findViewById(R.id.timerMs)).setText("." + milliseconds);
+    }
+
+    private Runnable startTimer = new Runnable() {
+        public void run() {
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this, REFRESH_RATE);
+        }
+    };
+
+    public void startClick(View view) {
+        showStopButton();
+        if (stopped) {
+            startTime = System.currentTimeMillis() - elapsedTime;
+        } else {
+            startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+    }
+
+    public void stopClick(View view) {
+        hideStopButton();
+        mHandler.removeCallbacks(startTimer);
+        stopped = true;
+    }
+
+    public void resetClick(View view) {
+        stopped = false;
+        ((TextView) findViewById(R.id.timer)).setText("00:00:00");
+        ((TextView) findViewById(R.id.timerMs)).setText(".0");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
